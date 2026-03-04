dd6a26ebaacea6d9cd7da8bf7a8af23a9bb11efb
SHYuanBest <shyuan-cs@hotmail.com>
2026-03-04 03:31:47 +0000

    Initial commit

---
+++ scripts/inference/helios-base_v2v.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0 python infer_helios.py \
+    --base_model_path "BestWishYsh/Helios-Base" \
+    --transformer_path "BestWishYsh/Helios-Base" \
+    --sample_type "v2v" \
+    --video_path "example/car.mp4" \
+    --prompt "A bright yellow Lamborghini Huracn Tecnica speeds along a curving mountain road, surrounded by lush green trees under a partly cloudy sky. The car's sleek design and vibrant color stand out against the natural backdrop, emphasizing its dynamic movement. The road curves gently, with a guardrail visible on one side, adding depth to the scene. The motion blur captures the sense of speed and energy, creating a thrilling and exhilarating atmosphere. A front-facing shot from a slightly elevated angle, highlighting the car's aggressive stance and the surrounding greenery." \
+    --guidance_scale 5.0 \
+    --enable_compile \
+    --output_folder "./output_helios/helios-base"
+
+
+    # --use_cfg_zero_star \
+    # --use_zero_init \
+    # --zero_steps 1 \
---
+++ scripts/inference/helios-distilled_v2v.sh
@@ -0,0 +1,16 @@
+CUDA_VISIBLE_DEVICES=0 python infer_helios.py \
+    --base_model_path "BestWishYsh/Helios-Distilled" \
+    --transformer_path "BestWishYsh/Helios-Distilled" \
+    --sample_type "v2v" \
+    --video_path "example/car.mp4" \
+    --prompt "A bright yellow Lamborghini Huracn Tecnica speeds along a curving mountain road, surrounded by lush green trees under a partly cloudy sky. The car's sleek design and vibrant color stand out against the natural backdrop, emphasizing its dynamic movement. The road curves gently, with a guardrail visible on one side, adding depth to the scene. The motion blur captures the sense of speed and energy, creating a thrilling and exhilarating atmosphere. A front-facing shot from a slightly elevated angle, highlighting the car's aggressive stance and the surrounding greenery." \
+    --num_frames 240 \
+    --guidance_scale 1.0 \
+    --is_enable_stage2 \
+    --pyramid_num_inference_steps_list 2 2 2 \
+    --is_amplify_first_chunk \
+    --enable_compile \
+    --output_folder "./output_helios/helios-distilled"
+
+
+    # --pyramid_num_inference_steps_list 1 1 1 \
